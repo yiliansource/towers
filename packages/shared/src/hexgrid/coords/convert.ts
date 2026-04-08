935d3d75ea8b4f2cbeb2f2ed59c13a087c30ab74
Ian Hornik <yiliansource@gmail.com>
2026-04-08 12:23:33 +0200

    fix build

--- packages/shared/src/hexgrid/coords/convert.ts
+++ packages/shared/src/hexgrid/coords/convert.ts
@@ -2,10 +2,10 @@ import { Axial, Cube } from "../types.js";
 import { axial } from "./axial.js";
 import { cube } from "./cube.js";
 
-export function cubeToAxial(cube: Cube) {
-    return axial(cube.q, cube.r);
-}
-
 export function axialToCube(hex: Axial) {
     return cube(hex.q, hex.r, -hex.q - hex.r);
+}
+
+export function cubeToAxial(cube: Cube) {
+    return axial(cube.q, cube.r);
 }
